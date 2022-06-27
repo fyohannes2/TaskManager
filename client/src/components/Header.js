3253c88b477f24b5adb82bb7a1c52c3bf435cd00
Filmon <filmon@Lydias-Air.attlocal.net>
2022-06-27 17:40:52 -0400

    first commit

---
+++ client/src/components/Header.js
@@ -0,0 +1,15 @@
+import { Navbar, Container } from 'react-bootstrap';
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar bg="light" >
+                <Container className="justify-content-center">
+                <Navbar.Brand>"That Simple Task Manager Web App" </Navbar.Brand>
+                </Container>
+            </Navbar>
+        </div>
+    )
+}
+
+export default Header
