f82dceccc442db0e8e67814ab150f928eb9ea35d
Filmon <filmon@Lydias-Air.attlocal.net>
2022-06-27 20:13:49 -0400

    deploy

--- server/app.js
+++ server/app.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+require("dotenv").config();
 const crudRoutes = require('./routes/crud');
 
 const app = express();
-const MONGODBURL = "mongodb+srv://admin:abc1234@clusterb.t5btr.mongodb.net/?retryWrites=true&w=majority"
+const MONGODB_URI = process.env.MONGODB_URI
+const PORT = process.env.PORT || 8080;
+mongoose.connect(MONGODB_URI,{useNewUrlParser: true, useUnifiedTopology: true}) 
+// Connection Events
+mongoose.connection
+.on('connected', () => console.log('connected to MongoDB'))
+.on('error', (err) => console.log('Error with MongoDB: ' + err.message));
+  
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -26,11 +33,6 @@ app.use((error, req, res, next) => {
     });
 
 });
-
-mongoose.connect(MONGODBURL)
-    .then(result => {        
-        app.listen(process.env.PORT || 8080);
-    })
-    .catch(error => {
-        console.log(error);
-    });
+app.listen(PORT, () => {
+    console.log(`Express is listening on port: ${PORT}`)
+});
