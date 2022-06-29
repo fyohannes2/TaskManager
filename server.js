0f0a50e2d56db41763ce7d0d4709bba07eecd22f
Filmon <filmon@Lydias-Air.attlocal.net>
2022-06-29 14:23:23 -0400

    deploy

--- server.js
+++ server.js
@@ -1,26 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 require("dotenv").config();
 const crudRoutes = require('./routes/crud');
 
 const app = express();
 const MONGODB_URI = process.env.MONGODB_URI
 const PORT = process.env.PORT || 8080;
-mongoose.connect(MONGODB_URI,{useNewUrlParser: true, useUnifiedTopology: true}) 
+mongoose.connect(MONGODB_URI) 
 // Connection Events
+
+// Mongo Status Listeners
 mongoose.connection
-.on('connected', () => console.log('connected to MongoDB'))
-.on('error', (err) => console.log('Error with MongoDB: ' + err.message));
+.on('connected', () => console.log('Connected to MongoDB'))
+.on('error', (err) => console.log('Error with MongoDB: ' + err.message))
+
+
   
 app.use(express.json());
+app.use(cors());
 
-app.use((req, res, next) => {
 
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
+
 
 app.use(crudRoutes);
 
